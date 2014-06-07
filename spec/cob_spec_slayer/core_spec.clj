befc5648d0f743dae7eed5c3abbb8c2735af3676
alexreedhill <ahill1994@student.elgin.edu>
2014-06-07 12:03:06 -0500

    Param decode passing when server started in repl

--- spec/cob_spec_slayer/core_spec.clj
+++
@@ -1,7 +0,0 @@
-(ns cob-spec-slayer.core-spec
-  (:require [speclj.core :refer :all]
-            [cob-spec-slayer.core :refer :all]))
-
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
--- src/cob_spec_slayer/core.clj
+++ src/cob_spec_slayer/core.clj
@@ -1,10 +1,21 @@
 (ns cob-spec-slayer.core
   (:require [lazy-server.server :as server]
-            [lazy-server.router :refer [defrouter]])
+            [lazy-server.router :refer [defrouter GET]]
+            [clojure.string :refer [join]])
   (:gen-class :main true))
 
-(defrouter cob-spec-router
-  (GET "/" {:body "" :code 200}))
+(defn param-decode-response [request]
+  (join \newline
+        [(str
+           (name (first (second (request :query-params)))) " = "
+           (second (second (request :query-params))))
+         (str
+           (name (first (first (request :query-params)))) " = "
+           (second (first (request :query-params))))]))
+
+(defrouter cob-spec-router request
+  (GET "/" {:body "" :code 200})
+  (GET "/parameters" {:body (param-decode-response request) :code 200}))
 
 (defn -main [& args]
   (server/-main "5000" "localhost" cob-spec-router))
