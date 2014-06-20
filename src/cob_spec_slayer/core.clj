41321b38dff2a81c389ccc489a5f38bb9ad933cd
alexreedhill <ahill1994@student.elgin.edu>
2014-06-20 14:54:43 -0500

    patch with etag

--- src/cob_spec_slayer/core.clj
+++ src/cob_spec_slayer/core.clj
@@ -1,7 +1,6 @@
 (ns cob-spec-slayer.core
   (:require [lazy-server.server :as server]
             [lazy-server.router :refer :all]
-            [lazy-server.response-builder :refer [redirect serve-file]]
             [lazy-server.basic-authenticator :refer [basic-auth]]
             [clojure.string :refer [join]]
             [clojure.java.io :refer [as-relative-path]])
@@ -21,6 +20,7 @@
   (GET "/logs" (basic-auth request "Authentication required" (slurp "public/log.txt") "admin:hunter2"))
   (POST "/form" (save-resource request))
   (PUT "/form" (save-resource request))
+  (PATCH "/patch-content.txt" (save-resource request))
   (OPTIONS "/method_options" {:code 200})
   (not-found "Sorry, there's nothing here!"))
 
