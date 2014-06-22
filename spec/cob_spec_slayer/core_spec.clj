5861e009ff58eab5c95dee8c21678ae1001cfdba
alexreedhill <ahill1994@student.elgin.edu>
2014-06-22 13:01:50 -0500

    refactor directory links response

--- spec/cob_spec_slayer/core_spec.clj
+++ spec/cob_spec_slayer/core_spec.clj
@@ -1,11 +1,18 @@
 (ns cob-spec-slayer.core-spec
   (:require [cob-spec-slayer.core :refer :all]
+            [lazy-server.file-interactor :refer [directory-contents]]
+            [lazy-server.spec-helper :refer [bytes-to-string]]
             [clojure.java.io :refer [delete-file]]
             [speclj.core :refer :all]))
 
 (describe "cob spec slayer"
-
   (it "formats parameter decoder response"
     (let [request {:query-params {"baz" "snazz" "foo" "bar"}}]
       (should= "baz = snazz\nfoo = bar"
-        (param-decode-response request)))))
+        (param-decode-response request))))
+
+  (it "formats directory links response"
+    (with-redefs [directory-contents (fn [_] '("test.txt", "test.gif"))]
+      (should= (str "<a href=\"/test.txt\">test.txt</a><br>\n"
+                    "<a href=\"/test.gif\">test.gif</a><br>\n")
+        (bytes-to-string (directory-response))))))
--- src/cob_spec_slayer/core.clj
+++ src/cob_spec_slayer/core.clj
@@ -2,19 +2,25 @@
   (:require [lazy-server.server :as server]
             [lazy-server.router :refer :all]
             [lazy-server.basic-authenticator :refer [basic-auth]]
-            [clojure.string :refer [join]]
-            [clojure.java.io :refer [as-relative-path]])
+            [lazy-server.file-interactor :refer [directory-contents]]
+            [clojure.string :refer [trim-newline]])
   (:gen-class :main true))
 
 (defn param-decode-response [request]
-  (str
-    (name (first (second (request :query-params)))) " = "
-    (second (second (request :query-params))) "\n"
-    (name (first (first (request :query-params)))) " = "
-    (second (first (request :query-params)))))
+  (->> (request :query-params)
+       (sort)
+       (map #(str (key %) " = " (val %) "\n"))
+       (apply str)
+       (trim-newline)))
+
+(defn directory-response []
+  (->> (directory-contents public-dir)
+       (map #(str "<a href=\"/" % "\">" % "</a><br>\n"))
+       (mapcat #(.getBytes %))
+       (byte-array)))
 
 (defrouter cob-spec-router request
-  (GET "/" {:body "" :code 200})
+  (GET "/" {:code 200 :body (cob-spec-slayer.core/directory-response)})
   (GET "/parameters" {:body (cob-spec-slayer.core/param-decode-response request) :code 200})
   (GET "/redirect" (redirect "http://localhost:5000/"))
   (GET "/logs" (basic-auth request "Authentication required" (slurp "public/log.txt") "admin:hunter2"))
@@ -25,4 +31,4 @@
   (not-found "Sorry, there's nothing here!"))
 
 (defn -main [& args]
-  (server/-main "5000" "localhost" cob-spec-router))
+  (server/-main "5000" "localhost" cob-spec-router "public/"))
