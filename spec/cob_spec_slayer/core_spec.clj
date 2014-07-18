edae9fec8f0e979d0ed7c3cd3ced1afbd86f8a92
alexreedhill <ahill1994@student.elgin.edu>
2014-07-18 09:55:02 -0500

    WIP WIP WIP

--- spec/cob_spec_slayer/core_spec.clj
+++ spec/cob_spec_slayer/core_spec.clj
@@ -9,10 +9,10 @@
   (it "formats parameter decoder response"
     (let [request {:query-params {"baz" "snazz" "foo" "bar"}}]
       (should= "baz = snazz\nfoo = bar"
-        (param-decode-response request))))
+        (param-decode-body request))))
 
   (it "formats directory links response"
     (with-redefs [directory-contents (fn [_] '("test.txt", "test.gif"))]
       (should= (str "<a href=\"/test.txt\">test.txt</a><br>\n"
                     "<a href=\"/test.gif\">test.gif</a><br>\n")
-        (bytes-to-string (directory-response))))))
+        (bytes-to-string (directory-body))))))
--- src/cob_spec_slayer/core.clj
+++ src/cob_spec_slayer/core.clj
@@ -1,29 +1,35 @@
 (ns cob-spec-slayer.core
   (:require [lazy-server.server :as server]
             [lazy-server.router :refer :all]
-            [lazy-server.basic-authenticator :refer [basic-auth]]
+            [lazy-server.basic-authenticator :refer [authenticate]]
             [lazy-server.file-interactor :refer [directory-contents]]
             [clojure.string :refer [trim-newline]])
   (:gen-class :main true))
 
-(defn param-decode-response [request]
+(defn param-decode-body [request]
   (->> (request :query-params)
        (sort)
        (map #(str (key %) " = " (val %) "\n"))
        (apply str)
        (trim-newline)))
 
-(defn directory-response []
+(defn param-decode-response [request]
+  {:code 200 :body (param-decode-body request)})
+
+(defn directory-body []
   (->> (directory-contents public-dir)
        (map #(str "<a href=\"/" % "\">" % "</a><br>\n"))
        (mapcat #(.getBytes %))
        (byte-array)))
 
+(defn directory-response
+  {:code 200 :body (directory-body)})
+
 (defrouter cob-spec-router request
-  (GET "/" {:code 200 :body (cob-spec-slayer.core/directory-response)})
-  (GET "/parameters" {:body (cob-spec-slayer.core/param-decode-response request) :code 200})
+  (GET "/" (cob-spec-slayer.core/directory-response))
+  (GET "/parameters" (cob-spec-slayer.core/param-decode-response request) :code 200)
   (GET "/redirect" (redirect "http://localhost:5000/"))
-  (GET "/logs" (basic-auth request "Authentication required" (slurp "public/log.txt") "admin:hunter2"))
+  (GET "/logs" (authenticate request "Authentication required" (slurp "public/log.txt") "admin:hunter2"))
   (POST "/form" (save-resource request))
   (PUT "/form" (save-resource request))
   (PATCH "/patch-content.txt" (save-resource request))
